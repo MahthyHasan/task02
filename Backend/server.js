b8c5290ff2894b8bc8160007ad9f6269f96fed4e
Mahthy Hasan <mahthyhasan01@gmail.com>
2025-03-17 18:33:05 +0530

    2-Creating Connection To Databse and user Authentications

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,26 @@
-console.log("Server is Running")
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5005;
+const corsOptions = {
+    origin: 'http://localhost:3001', 
+    credentials: true, 
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+    console.log('Server started at http://localhost:5005');
+    connectMongoDB();
+})
